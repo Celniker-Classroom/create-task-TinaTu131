0341697cd0c8393b435bc38ab7cb56fadc0047b0
TinaTu131 <ttu73580@gmail.com>
2026-04-14 04:51:38 +0000

    final version

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 // add javascript here
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Website loaded successfully");
+    const body=document.querySelector("body");
+    body.addEventListener("click", function (){
+        console.log ("Body clicked");
+    });
+});
